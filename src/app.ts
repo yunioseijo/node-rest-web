213f1c60c60767119c5dfc2e2720070ac551ce0f
yunioseijo <yunioseijo@gmail.com>
2025-08-03 19:29:15 +0200

    Http2 y SSL

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,43 @@
 import fs from "fs";
-import http from "http";
+import http2 from "http2";
 
-const server = http.createServer((req, res) => {
-  console.log(req.url);
+const server = http2.createSecureServer(
+  {
+    key: fs.readFileSync("./keys/server.key"),
+    cert: fs.readFileSync("./keys/server.crt"),
+  },
+  (req, res) => {
+    console.log(req.url);
 
-  //   res.writeHead(200, { "Content-Type": "text/html" });
-  //   res.write(`<h1> URL ${req.url} </h1>`);
-  //   res.end();
+    //   res.writeHead(200, { "Content-Type": "text/html" });
+    //   res.write(`<h1> URL ${req.url} </h1>`);
+    //   res.end();
 
-  //   const data = { name: "John", age: 30, city: "New York" };
-  //   res.writeHead(200, { "Content-Type": "application/json" });
-  //   res.end(JSON.stringify(data));
+    //   const data = { name: "John", age: 30, city: "New York" };
+    //   res.writeHead(200, { "Content-Type": "application/json" });
+    //   res.end(JSON.stringify(data));
 
-  if (req.url === "/") {
-    const htmlFile = fs.readFileSync("./public/index.html", "utf-8");
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(htmlFile);
-    return;
+    if (req.url === "/") {
+      const htmlFile = fs.readFileSync("./public/index.html", "utf-8");
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(htmlFile);
+      return;
+    }
+    if (req.url?.endsWith(".js")) {
+      res.writeHead(200, { "Content-Type": "text/javascript" });
+    } else if (req.url?.endsWith(".css")) {
+      res.writeHead(200, { "Content-Type": "text/css" });
+    }
+
+    try {
+      const responseContent = fs.readFileSync(`./public${req.url}`, "utf-8");
+      res.end(responseContent);
+    } catch (error) {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end();
+    }
   }
-  if (req.url?.endsWith(".js")) {
-    res.writeHead(200, { "Content-Type": "text/javascript" });
-  } else if (req.url?.endsWith(".css")) {
-    res.writeHead(200, { "Content-Type": "text/css" });
-  }
-  const responseContent = fs.readFileSync(`./public${req.url}`, "utf-8");
-  if (responseContent) {
-    res.end(responseContent);
-  } else {
-    res.writeHead(404, { "Content-Type": "text/html" });
-    res.end("<h1>404 Not Found</h1>");
-  }
-});
+);
 server.listen(8080, () => {
   console.log("Server running on port 8080");
 });
