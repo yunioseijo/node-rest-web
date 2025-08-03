153ab16d4eae3bea3e9f618becccb41d216d464d
yunioseijo <yunioseijo@gmail.com>
2025-08-03 17:12:17 +0200

    webserver http/1

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
-const saludo: string = "Hello World";
+import http from "http";
 
-console.log(saludo);
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+
+  res.write("hello world");
+  res.end;
+});
+server.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
