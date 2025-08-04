17a937f29a2e2f0631d2dde12b82de7edd1dd340
yunioseijo <yunioseijo@gmail.com>
2025-08-04 18:57:32 +0200

    CRUD  -- Read

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,19 +1,24 @@
 import { Request, Response } from "express";
+const todos = [
+  { id: 1, title: "Todo 1", description: "Description 1", completed: true },
+  { id: 2, title: "Todo 2", description: "Description 2", completed: false },
+  { id: 3, title: "Todo 3", description: "Description 3", completed: true },
+];
 
 export class TodoController {
   //* DI
   constructor() {}
 
   public getTodos = (req: Request, res: Response) => {
-    return res.json([
-      { id: 1, title: "Todo 1", description: "Description 1", completed: true },
-      {
-        id: 2,
-        title: "Todo 2",
-        description: "Description 2",
-        completed: false,
-      },
-      { id: 3, title: "Todo 3", description: "Description 3", completed: true },
-    ]);
+    return res.json(todos);
+  };
+  public getTodoById = (req: Request, res: Response) => {
+    const { id } = req.params;
+    if (isNaN(Number(id)))
+      return res.status(400).json({ error: "ID argument is not a number" });
+    const todo = todos.find((todo) => todo.id === Number(id));
+    todo
+      ? res.json(todo)
+      : res.status(404).json({ error: `Todo with id ${id} not found` });
   };
 }
--- src/presentation/todos/routes.ts
+++ src/presentation/todos/routes.ts
@@ -6,6 +6,7 @@ export class TodoRoutes {
     const router = Router();
     const todoController = new TodoController();
     router.get("/", todoController.getTodos);
+    router.get("/:id", todoController.getTodoById);
     return router;
   }
 }
