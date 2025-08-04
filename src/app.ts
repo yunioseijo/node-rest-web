1572f3ed41173f3b65e04f17486fa65013e4deec
yunioseijo <yunioseijo@gmail.com>
2025-08-04 12:12:59 +0200

    Servir SPA con Router Variables de entorno

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { envs } from "./config/envs";
 import { Server } from "./presentation/server";
 
 (() => {
@@ -5,6 +6,6 @@ import { Server } from "./presentation/server";
 })();
 
 function main() {
-  const server = new Server();
+  const server = new Server({ port: envs.PORT, public_path: envs.PUBLIC_PATH });
   server.start();
 }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,17 +1,42 @@
 import express from "express";
+import path from "path";
+
+interface Options {
+  port: number;
+  public_path?: string;
+}
 
 export class Server {
   private app = express();
-  constructor() {}
+  private readonly port: number;
+  private readonly public_path?: string;
+
+  constructor(options: Options) {
+    const { port, public_path = "public" } = options;
+    this.port = port;
+    this.public_path = public_path;
+  }
 
   async start() {
     //* Middlewares
 
     //* Public Folder
+    this.app.use(express.static(this.public_path!));
 
-    this.app.use(express.static("public"));
+    //SPA routes
+    this.app.get(/.*/, (req, res) => {
+      const indexPth = path.join(
+        process.cwd(),
+        `${this.public_path}`,
+        "index.html"
+      );
+      res.sendFile(indexPth);
+    });
+    // this.app.use((req, res) => {
+    //   res.sendFile(path.join(process.cwd(), "public", "index.html"));
+    // });
 
-    this.app.listen(3000);
-    console.log("Server running on port 3000");
+    this.app.listen(this.port);
+    console.log(`Server running on port ${this.port}`);
   }
 }
